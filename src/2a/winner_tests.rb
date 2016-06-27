bf903f22a9284da85027e8619e20926a5a5dfadc
Sergey Kazantsev <sergey.kazantsev1@gmail.com>
2016-06-27 23:35:55 +0100

    added some tests to 2a

--- src/2a/winner_tests.rb
+++ src/2a/winner_tests.rb
@@ -7,17 +7,22 @@ class WinnerCalculatorTests < Test::Unit::TestCase
     assert_equal('andrew', winner)
   end
 
+  test 'player who has max points wins' do
+    winner = WinnerCalculator.get_winner(['andrew -1', 'mike 2', 'andrew 4'])
+    assert_equal('andrew', winner)
+  end
+
   test 'line breaks in input' do
     winner = WinnerCalculator.get_winner(["mike 3\n"])
     assert_equal('mike', winner)
   end
 
-  test 'player who reaches max score first does not win' do
+  test 'player who reaches max score first loses' do
     winner = WinnerCalculator.get_winner(['mike 3', 'mike -1', 'andrew 3'])
     assert_equal('andrew', winner)
   end
 
-  test 'player who reaches max score first wins after losing some points' do
+  test 'player who reaches max score first wins even after losing some points' do
     winner = WinnerCalculator.get_winner(['mike 3', 'mike -1', 'andrew 2'])
     assert_equal('mike', winner)
   end
@@ -32,7 +37,7 @@ class WinnerCalculatorTests < Test::Unit::TestCase
     assert_equal('mike', winner)
   end
 
-  test 'both players lost scores and wins first who scored first' do
+  test 'both players lost scores and wins one who scored first' do
     winner = WinnerCalculator.get_winner(['andrew 5', 'mike 6', 'andrew -2', 'mike -3'])
     assert_equal('andrew', winner)
   end
